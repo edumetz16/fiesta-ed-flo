bd59122defb16c6e6614ca6a47524efc76fcd57e
edumetz16 <eduardo@linead.com.ar>
2025-02-18 14:07:15 -0300

    fix invitees write

--- src/app/api/assistants/route.ts
+++ src/app/api/assistants/route.ts
@@ -6,10 +6,13 @@ import { NextRequest, NextResponse } from "next/server";
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
   if(!body.code) return NextResponse.error();
-  const invitees = await get('invitees') as Invitee[];
-  const invitee = invitees.find((i) => i.code === body.code);
-  if(!invitee) return NextResponse.error();
-  invitee.assists = body.quantity || 0;
+  let invitees = await get('invitees') as Invitee[];
+  invitees = invitees.map((invitee) => {
+    if (invitee.code === body.code) {
+      invitee.assists = body.quantity || 0;
+    }
+    return invitee;
+  })
   
   setItem('invitees', invitees);
   return NextResponse.json({success: true, assists: body.assists});
