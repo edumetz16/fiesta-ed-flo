9afd62eabb470e2594d6a79ef5700f6b9e477767
edumetz16 <eduardo@linead.com.ar>
2025-02-18 22:10:05 -0300

    return value on getConfigValue

--- src/services/firestore.ts
+++ src/services/firestore.ts
@@ -2,28 +2,20 @@ import {initializeApp} from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { cert, getApp, getApps } from "firebase-admin/app";
 
+const app = getApps().length > 0 ? getApp() : initializeApp({credential: cert(JSON.parse(process.env.FIREBASE_CREDENTIALS!))});
+
+const db = getFirestore(app);
+
+
 export const saveInviteeResponse = async ({code, quantity, assists}: {code: string, quantity: number, assists: boolean}) =>{
-  const app = getFirebaseApp();
-  const db = getFirestore(app);
   await db.collection('invitees').doc(code).set({code, quantity, assists}, {merge: true});
 }
 
 export const saveConfigValue = async (key: string, value: unknown) =>{ 
-  const app = getFirebaseApp();
-  const db = getFirestore(app);
   await db.collection('config').doc(key).set({key, value}, {merge: true});
 }
 
 export const getConfigValue = async (key: string): Promise<unknown> => {
-  const app = getFirebaseApp();
-  const db = getFirestore(app);
   const doc = await db.collection('config').doc(key).get();
-  return doc.data();
-}
-
-const getFirebaseApp = () =>{
-  if(getApps().length > 0) return getApp();
-  else return initializeApp({
-    credential: cert(JSON.parse(process.env.FIREBASE_CREDENTIALS!)),
-  })
+  return doc.data()?.value;
 }
